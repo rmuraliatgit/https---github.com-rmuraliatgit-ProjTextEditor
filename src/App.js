fce80f62950a31cc8eca250867972869f3510ea6
rmuraliatgit <muraliravi98@gmail.com>
2021-09-16 14:40:30 +0530

    new commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import { CKEditor } from '@ckeditor/ckeditor5-react';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import parse from 'html-react-parser'
 function App() {
+  const[text, setText] = useState('') 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="editor">
+        <CKEditor 
+          editor = {ClassicEditor}
+          data = {text}
+          onChange = {(event, editor) => { const data = editor.getData()
+          setText(data)}}
+          />
+      </div>
+      <div>
+        <h2>Content</h2>
+        <p>{parse(text)}</p>
+      </div>
     </div>
   );
 }
